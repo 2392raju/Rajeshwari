7d83a9baf29b701ad56c5c2b2353f4c2b7df0711
2392raju <raju2392@gmail.com>
2014-05-05 20:29:55 +0530

    Update check_stat.php

--- project/check_stat.php
+++ project/check_stat.php
@@ -1,9 +1,9 @@
 <?php
-// Make a MySQL Connection
-mysql_connect("mysql14.000webhost.com", "a6428231_comp", "123abc") or die(mysql_error());
+mysql_connect("localhost", "root", "root") or die(mysql_error());
 
 // Select the database
-mysql_select_db("a6428231_comp") or die(mysql_error());
+mysql_select_db("smart") or die(mysql_error());
+
 $val=$_POST['complaintno'];
 
 $result = mysql_query("SELECT * FROM complaint_images_updated where complaint_no='$val'") or die(mysql_error());
